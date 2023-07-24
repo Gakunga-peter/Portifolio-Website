4a82161b68770fa59ba2f3a8165f62f576167454
Gakunga Peter <gakungakyle@gmail.com>
2023-07-24 20:50:25 +0300

    change image.me and activation of the home link in the nav bar and footer

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -11,7 +11,7 @@ const Nav = () => {
   const [activeNav, setActiveNav] =useState('#')
   return (
     <nav>
-      <a href="#" onClick={()=>setActiveNav('#')} className={activeNav === '#' ? 'active' : '' }> <AiFillHome/> </a>
+      <a href="#header" onClick={()=>setActiveNav('#')} className={activeNav === '#' ? 'active' : '' }> <AiFillHome/> </a>
       <a href="#about" onClick={()=>setActiveNav('#about')} className={activeNav === '#about' ? 'active' : '' }> <AiOutlineUser/> </a>
       <a href="#experience"onClick={()=>setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : '' }><BsBookHalf/> </a>
       <a href="#services" onClick={()=>setActiveNav('#service')} className={activeNav === '#service' ? 'active' : '' }> <FcServices/> </a>
