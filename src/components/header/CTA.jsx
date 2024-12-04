4ab7dbd82eda4c351ce81961482f1bd40e0e7c55
Peter Gakunga <137217372+Gakunga-peter@users.noreply.github.com>
2024-12-04 14:31:12 +0300

    Update CTA.jsx
    CV and Resume Update

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,6 +5,7 @@ const CTA = () => {
   return (
     <div className='cta'>
         <a href={CV} download className='btn'>Download CV</a>
+        <a href={Resume} download className='btn'>Download Resume</a>
         <a href="#contact" className='btn btn-primary'>Contact me</a>
     </div>
   )
