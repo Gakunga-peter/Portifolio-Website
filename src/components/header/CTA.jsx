b5b4bbd65ea56044145a852092e741e284994b9a
Peter Gakunga <137217372+Gakunga-peter@users.noreply.github.com>
2024-12-04 14:40:57 +0300

    Update CTA.jsx
    Adding a resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <div className='cta'>
         <a href={CV} download className='btn'>Download CV</a>
-        <a href={Resume} download className='btn'>Download Resume</a>
+        <a href={Gakunga_P_Kiarie_Resume} download className='btn'>Download Resume</a>
         <a href="#contact" className='btn btn-primary'>Contact me</a>
     </div>
   )
